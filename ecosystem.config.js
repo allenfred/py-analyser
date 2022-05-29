2b5cc97f8d5bd3741dbd2ff5d9923c9a70f2bd4f
Allen.F.Cheng <allen.f.cheng@pwc.com>
2022-05-29 21:40:27 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'klines_CN',
-      log_date_format：'YYYY-MM-DD HH:mm:ss',
+      log_date_format：'YYYY-MM-DD HH:mm Z',
       script: 'jobs/candles/daily/cn.py',
       interpreter: 'python3',
       instances: 1,
@@ -13,7 +13,7 @@ module.exports = {
     },
     {
       name: 'cn-dividend',
-      log_date_format：'YYYY-MM-DD HH:mm:ss',
+      log_date_format：'YYYY-MM-DD HH:mm Z',
       script: 'jobs/candles/daily/cn_dividend.py',
       interpreter: 'python3',
       instances: 1,
@@ -24,7 +24,7 @@ module.exports = {
     },
     {
       name: 'klines_HK',
-      log_date_format：'YYYY-MM-DD HH:mm:ss',
+      log_date_format：'YYYY-MM-DD HH:mm Z',
       script: 'jobs/candles/daily/hk.py',
       interpreter: 'python3',
       instances: 1,
@@ -35,7 +35,7 @@ module.exports = {
     },
     {
       name: 'klines_US',
-      log_date_format：'YYYY-MM-DD HH:mm:ss',
+      log_date_format：'YYYY-MM-DD HH:mm Z',
       script: 'jobs/candles/daily/us.py',
       interpreter: 'python3',
       instances: 1,
@@ -46,7 +46,7 @@ module.exports = {
     },
     {
       name: 'cn-weekly',
-      log_date_format：'YYYY-MM-DD HH:mm:ss',
+      log_date_format：'YYYY-MM-DD HH:mm Z',
       script: 'jobs/candles/weekly/analysis.py',
       interpreter: 'python3',
       instances: 1,
@@ -57,7 +57,7 @@ module.exports = {
     },
     {
       name: 'cn-basic',
-      log_date_format：'YYYY-MM-DD HH:mm:ss',
+      log_date_format：'YYYY-MM-DD HH:mm Z',
       script: 'jobs/candles/daily/cn_basic.py',
       interpreter: 'python3',
       instances: 1,
@@ -68,7 +68,7 @@ module.exports = {
     },
     {
       name: 'analysis_CN',
-      log_date_format：'YYYY-MM-DD HH:mm:ss',
+      log_date_format：'YYYY-MM-DD HH:mm Z',
       script: 'jobs/analysis/daily/cn.py',
       interpreter: 'python3',
       instances: 1,
@@ -79,7 +79,7 @@ module.exports = {
     },
     {
       name: 'analysis_HK',
-      log_date_format：'YYYY-MM-DD HH:mm:ss',
+      log_date_format：'YYYY-MM-DD HH:mm Z',
       script: 'jobs/analysis/daily/hk.py',
       interpreter: 'python3',
       instances: 1,
@@ -90,7 +90,7 @@ module.exports = {
     },
     {
       name: 'analysis_US',
-      log_date_format：'YYYY-MM-DD HH:mm:ss',
+      log_date_format：'YYYY-MM-DD HH:mm Z',
       script: 'jobs/analysis/daily/us.py',
       interpreter: 'python3',
       instances: 1,
