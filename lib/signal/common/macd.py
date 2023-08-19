46d5227b948feadf2a69d08379247d9849c81b39
allenfc <forchengfei@gmail.com>
2023-08-19 10:41:45 +0800

    更新 macd 因子

--- lib/signal/common/macd.py
+++ lib/signal/common/macd.py
@@ -10,7 +10,7 @@ def is_macd_zero_gold_cross(index, df):
     diff = df['diff'].to_numpy()
     dea = df['dea'].to_numpy()
 
-    return diff[index - 1] < dea[index - 1] and diff[index] > dea[index] \
+    return diff[index - 1] <= dea[index - 1] and diff[index] > dea[index] \
            and 1 > diff[index] > dea[index] > 0
 
 
@@ -25,4 +25,4 @@ def is_macd_gold_cross(index, df):
     diff = df['diff'].to_numpy()
     dea = df['dea'].to_numpy()
 
-    return diff[index - 1] < dea[index - 1] and diff[index] > dea[index]
+    return diff[index - 1] <= dea[index - 1] and diff[index] > dea[index]
